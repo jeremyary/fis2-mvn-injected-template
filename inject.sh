bd17cb4c7240ad517c441f998c6be7a0e85d48da
Jeremy Ary <jary@redhat.com>
2017-06-07 00:46:33 -0500

    .

--- inject.sh
+++ inject.sh
@@ -5,6 +5,6 @@ for d in $(find . -name 'pom.xml')
 do
     cd $(echo $d | tr -d '\r' | awk '{print $2}')
     echo "currently in $d..."
-    eval 'mvn dependency:go-offline'
+    eval 'mvn -Dmaven.repo.local=/tmp/artifacts/m2 -B dependency:go-offline'
 done
 echo "DONE W/SCRIPT"
