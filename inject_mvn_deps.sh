d2ad0847a785c8fd21d540fc79153805aba8237d
Jeremy Ary <jary@redhat.com>
2017-06-07 15:58:42 -0500

    altering project name and settings param

--- inject_mvn_deps.sh
+++ inject_mvn_deps.sh
@@ -6,7 +6,7 @@ do
     NAME=$(dirname "${d//\.\///tmp/}")
     cd "$NAME"
     echo "pom.xml file detected in: $NAME, fetching dependencies..."
-    mvn -s configuration/settings.xml -Dmaven.repo.local=/tmp/artifacts/m2 package -DskipTests -e -Dfabric8.skip=true -B
+    mvn -s /home/jboss/fis2-mvn-injected-template/configuration/settings.xml -Dmaven.repo.local=/tmp/artifacts/m2 package -DskipTests -e -Dfabric8.skip=true -B
     if [ "$?" -ne 0 ]; then
         echo "Maven packaging unsuccessful"
         exit 1
