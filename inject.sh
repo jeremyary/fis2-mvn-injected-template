e482b50627328f480624af138ccfd3253d8d2984
Jeremy Ary <jary@redhat.com>
2017-06-07 00:44:43 -0500

    .

--- inject.sh
+++ inject.sh
@@ -3,7 +3,7 @@
 echo "Starting maven dependency resolution..."
 for d in $(find . -name 'pom.xml')
 do
-    cd "$d"
+    cd $(echo $d | tr -d '\r' | awk '{print $2}')
     echo "currently in $d..."
     eval 'mvn dependency:go-offline'
 done
