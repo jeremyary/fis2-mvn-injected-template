9b0cc72f778939a0ba6cb9148ccb20c20f465460
Jeremy Ary <jary@redhat.com>
2017-06-07 00:39:45 -0500

    .

--- inject.sh
+++ inject.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "STARTING SCRIPT"
-echo $PWD
-echo $(ls -la)
+echo "Starting maven dependency resolution..."
 for d in $(find . -name 'pom.xml')
 do
-    echo "$d"
+    cd $d
+    echo "currently in $d..."
+    eval 'mvn dependency:go-offline'
 done
 echo "DONE W/SCRIPT"
