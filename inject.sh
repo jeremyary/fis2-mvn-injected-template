4c5cbda5951da5e8c71e6835379cc338ceb1dc5c
Jeremy Ary <jary@redhat.com>
2017-06-07 04:25:47 -0500

    .

--- inject.sh
+++ inject.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "Git project fetched. Starting maven dependency resolution..."
-for d in $(find . -depth 2 -name 'pom.xml')
+for d in $(find ./*/ -name 'pom.xml')
 do
     NAME=$(dirname "${d//\.\///tmp/}")
     cd "$NAME"
     echo "pom.xml file detected in: $NAME, fetching dependencies..."
-    eval 'mvn -Dmaven.repo.local=/tmp/artifacts/m3 package -DskipTests -e -Dfabric8.skip=true -B'
+    eval 'mvn -Dmaven.repo.local=/tmp/artifacts/m2 package -DskipTests -e -Dfabric8.skip=true -B'
 done
 echo "maven dependency resolution complete"
