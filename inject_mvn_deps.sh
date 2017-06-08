8f3c9d6c105d49daec7e6c0ca753f33708a1d1e0
Jeremy Ary <jary@redhat.com>
2017-06-08 14:43:30 -0500

    adding model injection

--- inject_mvn_deps.sh
+++ inject_mvn_deps.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
 echo "Git project fetched. Starting maven dependency resolution..."
+if [ -d "Model" ]; then
+  echo "Model module detected, injecting..."
+  cd Model
+  mvn -s /tmp/settings.xml -Dmaven.repo.local=/tmp/artifacts/m2 install -B
+  cd ../
+fi
 for d in $(find ./*/ -name 'pom.xml')
 do
     NAME=$(dirname "${d//\.\///tmp/}")
-    cd "$NAME"
-    echo "pom.xml file detected in: $NAME, fetching dependencies..."
-    mvn -s /tmp/settings.xml -Dmaven.repo.local=/tmp/artifacts/m2 package -DskipTests -e -Dfabric8.skip=true -B
-    if [ "$?" -ne 0 ]; then
-        echo "Maven packaging unsuccessful"
-        exit 1
+    if [[ ${NAME} != *Model* ]]; then
+      cd "$NAME"
+      echo "pom.xml file detected in: $NAME, fetching dependencies..."
+      mvn -s /tmp/settings.xml -Dmaven.repo.local=/tmp/artifacts/m2 package -DskipTests -e -Dfabric8.skip=true -B
+      if [ "$?" -ne 0 ]; then
+          echo "Maven packaging unsuccessful"
+          exit 1
+      fi
     fi
 done
 echo "maven dependency resolution complete"
